01630847cf0d1fcba57ad56e8b3513cdc6b3a03e
Nikita Belkin <belkinsrc@gmail.com>
2024-06-21 08:06:46 +0300

    config: add styling processing

--- config/webpack/buildLoaders.ts
+++ config/webpack/buildLoaders.ts
@@ -2,13 +2,18 @@ import { ModuleOptions } from 'webpack';
 import { BuildOptions } from 'config/types';
 
 const buildLoaders = (options: BuildOptions): ModuleOptions['rules'] => {
-  return [
-    {
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    },
-  ];
+  const ts = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  const styles = {
+    test: /\.css$/i,
+    use: ['style-loader', 'css-loader'],
+  };
+
+  return [ts, styles];
 };
 
 export { buildLoaders };
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,5 @@
+import './style.css';
+
 export function calc(a: number, b: number) {
   return a + b;
 }
